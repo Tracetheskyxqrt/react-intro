bfde65fd3863caa9f9f7c3eeb162957f810589e5
Maskin Dmitriy <fffatal0@gmail.com>
2020-09-14 21:45:12 +0300

    second commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://sun9-67.userapi.com/c857328/v857328452/35e6d/h9s6FwF4_60.jpg' width='500' height='200'></img>
+    </header>;
+
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Profile = () => {
+    return <div className='content'>
+        <img src='https://i.ytimg.com/vi/fnT5WyvD5Us/maxresdefault.jpg'></img>
+        <div>
+            ava + description
+    </div>
+        <div>
+            My posts
+      <div>
+                New post
+      </div>
+        </div>
+        <div>
+            post 1
+    </div>
+        <div>
+            post 2
+    </div>
+    </div>;
+}
+
+export default Profile;
