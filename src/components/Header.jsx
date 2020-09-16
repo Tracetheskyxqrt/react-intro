06ac125845e398a398180d08b31153049df7cf90
Maskin Dmitriy <fffatal0@gmail.com>
2020-09-16 21:32:47 +0300

    3d commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import s from './Header.module.css'
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={s.header}>
         <img src='https://sun9-67.userapi.com/c857328/v857328452/35e6d/h9s6FwF4_60.jpg' width='500' height='200'></img>
     </header>;
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
+import s from './Profile.module.css'
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
         <img src='https://i.ytimg.com/vi/fnT5WyvD5Us/maxresdefault.jpg'></img>
         <div>
             ava + description
-    </div>
-        <div>
-            My posts
-      <div>
-                New post
-      </div>
         </div>
         <div>
-            post 1
+            My posts
+            <div>
+                New post
+            </div>
+            <div className={s.posts}>
+                <div className ={s.item}>
+                    post 1
+                </div>
+                <div className ={s.item}>
+                    post 2
+                </div>
+            </div>
+        </div>
     </div>
-        <div>
-            post 2
-    </div>
-    </div>;
 }
 
 export default Profile;
