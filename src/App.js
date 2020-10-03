96ff57a34598894e82f48b9a6d10c352c2baa099
Maskin Dmitriy <fffatal0@gmail.com>
2020-10-04 01:16:18 +0300

    add dispatch

--- src/App.js
+++ src/App.js
@@ -19,8 +19,7 @@ function App(props) {
                     <Route path='/dialogs' render={() => <Dialogs state={props.state.dialogsPage}/>}/>
                     <Route path='/profile' render={() => <Profile
                         profilePage={props.state.profilePage}
-                        addPost={props.addPost}
-                        updateNewPostText={props.updateNewPostText}
+                        dispatch={props.dispatch}
                     />}/>
 
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import s from './MyPosts.module.css'
 import Post from './Post/Post';
-//import addPost from '../../../redux/state'
 
 const MyPosts = (props) => {
     let postsElements=props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
@@ -9,12 +8,13 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.addPost();
+        props.dispatch({type: 'ADD-POST'});
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.updateNewPostText(text);
+        let action = {type: 'UPDATE-NEW-POST-TEXT', newText: text };
+        props.dispatch(action);
     }
 
     return (
