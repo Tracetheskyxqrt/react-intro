a8166b335f9b4a974083e2350b246267e4675eed
Maskin Dmitriy <fffatal0@gmail.com>
2020-10-15 17:10:29 +0300

    add authorization

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import s from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
         <img src='https://sun9-67.userapi.com/c857328/v857328452/35e6d/h9s6FwF4_60.jpg' width='500' height='200'></img>
+
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login :
+                <NavLink to={'/login'}>Login</NavLink>}
+        </div>
     </header>;
 
 }
